8a627ae77a9b590547c5f0c7adfbf3cdad293247
googleKrakenPlex <iambagger10@gmail.com>
2020-03-20 17:03:01 +0300

    Разработка первой версии (3) - Добавлена динамическая подстройка под размер окна

--- Console-Physical-Ball/main.cpp
+++ Console-Physical-Ball/main.cpp
@@ -9,8 +9,13 @@ int main(void)
 	ball ballObj;
 	int delayValue = 10;
 
+	RECT tempRect = { 0 };
 	while(true)
 	{
+		GetWindowRect(thisWindowHWND, &tempRect);
+		max.x = tempRect.right - tempRect.left;
+		max.y = tempRect.bottom - tempRect.top - 70; //Динамически подстраиваемся под размер
+
 		ballObj.physTick();
 		eriseWindowsConsole();
 		ballObj.show(); //Очищаем, и сразу выводим снова. В угоду отсутвия мерцания
